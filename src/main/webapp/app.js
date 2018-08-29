9793dafe45d72074a27947562a79e02ff980f192
Érico Knapp Lutzer <erico.lutzer@softexpert.com>
2018-08-29 18:05:49 -0300

    Ajustes para permitir testes de integração e correção de fileLocking do jetty

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,11 +1,20 @@
 ﻿new Vue({
   el: '#app',
   data: {
-    teste: 'Mas olha só que coisa!'
+    teste: 'Mas olha só que coisa!',
+	customers: undefined
   },
   methods: {
 	  alertName: function() {
 		  alert('Seu nome é ' + this.teste);
+	  },
+	  getCustomers: function() {
+		  this.$http.get('api/customer').then(response => {
+			  this.customers = response.body;
+			  this.teste = 'Executei!';
+		  }, response => {
+			  this.customers = 'Error!'
+		  });
 	  }
   }
-});
+})
--- src/test/java/com/example/resources/RestTest.java
+++ src/test/java/com/example/resources/RestTest.java
@@ -1,15 +1,38 @@
 package com.example.resources;
 
-import javax.ws.rs.core.Application;
-
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
+import org.glassfish.jersey.test.DeploymentContext;
 import org.glassfish.jersey.test.JerseyTest;
+import org.glassfish.jersey.test.ServletDeploymentContext;
+import org.glassfish.jersey.test.TestProperties;
+import org.glassfish.jersey.test.grizzly.GrizzlyWebTestContainerFactory;
+import org.glassfish.jersey.test.spi.TestContainerException;
+import org.glassfish.jersey.test.spi.TestContainerFactory;
 
+import com.buildingapp.ContextListener;
+import com.buildingapp.JsonProvider;
 import com.buildingapp.RestApplication;
 
 public abstract class RestTest extends JerseyTest {
 
 	@Override
-	protected Application configure() {
-		return new RestApplication();
+	protected TestContainerFactory getTestContainerFactory() throws TestContainerException {
+		return new GrizzlyWebTestContainerFactory();
+	}
+
+	@Override
+	protected DeploymentContext configureDeployment() {
+
+		// for parallel tests
+		forceSet(TestProperties.CONTAINER_PORT, "0");
+
+		return ServletDeploymentContext.forServlet(new ServletContainer(new RestApplication()))
+				.addListener(ContextListener.class).build();
+	}
+
+	@Override
+	protected void configureClient(ClientConfig config) {
+		config.register(JsonProvider.class);
 	}
 }
