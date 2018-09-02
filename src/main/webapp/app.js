0cd9958f253ad8dbb1571bdd804d9e4f7230448b
Érico K. Lutzer <erico.klutzer@gmail.com>
2018-09-02 20:05:50 -0300

    Pequenas melhorias no código js

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,16 +1,16 @@
 ﻿Vue.http.options.root = '/api';
 
-Vue.filter('currency', function (value) {
-    if (typeof value !== "number") {
-        return value;
-    }
-    return new Intl.NumberFormat('pt-BR', {
-        style: 'currency',
-        currency: 'BRL',
+Vue.filter('currency', value => {
+	if (typeof value !== "number") {
+		return value;
+	}
+	return new Intl.NumberFormat('pt-BR', {
+		style: 'currency',
+		currency: 'BRL',
 		minimumFractionDigits: 2,
 		maximumFractionDigits: 2
-    }).format(value);
-})
+	}).format(value);
+});
 
 new Vue({
 	el: '#app',
@@ -23,20 +23,20 @@ new Vue({
 	methods: {
 		getVehicles() {
 			this.$http.get('veiculo').then(response => {
-				this.vehicles = response.body
-			})
+				this.vehicles = response.body;
+			});
 		},
 		calculate() {
 			this.$http.post('custoTransporte', this.calcData).then(response => {
-				this.result = response.body.result
+				this.result = response.body.result;
 				this.error = false;
 			}, response => {
-				this.result = response.body.message
+				this.result = response.body.message;
 				this.error = true;
-			})
+			});
 		}
 	},
 	beforeMount() {
-		this.getVehicles()
+		this.getVehicles();
 	}
-})
+});
